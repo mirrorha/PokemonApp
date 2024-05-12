a0b3a71c47da3ef0c3acc1da878a99b17ec82a2e
mirrorha <arsars1701@mail.ru>
2024-05-12 18:37:13 +0300

    Fetching Pokemons now is main-safe

--- app/src/main/java/com/vkinternship/pokemonapp/data/PokemonRemoteDataSource.kt
+++ app/src/main/java/com/vkinternship/pokemonapp/data/PokemonRemoteDataSource.kt
@@ -2,34 +2,48 @@ package com.vkinternship.pokemonapp.data
 
 import com.vkinternship.pokemonapp.data.model.PokemonDetails
 import com.vkinternship.pokemonapp.data.model.PokemonListItem
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
-
-
 class PokemonRemoteDataSource @Inject constructor(
     private val pokemonApi: PokemonApi,
+    private val ioDispatcher: CoroutineDispatcher
 ) {
 
-    suspend fun fetchPokemons() = pokemonApi
-            .fetchPokemons(200, 0)
-            .results
-            .map { pokemon ->
-                PokemonListItem(
-                    id = pokemon.url.split('/').let { it[it.size - 2].toInt() },
-                    name = pokemon.name
-                )
-            }
+    suspend fun fetchPokemons() =
 
-    suspend fun fetchPokemonImageUrl(id: Int) = pokemonApi
-        .fetchPokemon(id)
-        .sprites
-        .front_default
+        withContext(ioDispatcher) {
+            pokemonApi
+                .fetchPokemons(200, 0)
+                .results
+                .map { pokemon ->
+                    PokemonListItem(
+                        id = pokemon.url.split('/').let { it[it.size - 2].toInt() },
+                        name = pokemon.name
+                    )
+                }
+        }
 
-    suspend fun fetchPokemon(id: Int) = pokemonApi
-        .fetchPokemon(id)
-        .let {
-            PokemonDetails(
-                name = it.name,
-                defaultUrl = it.sprites.front_default
-            )
+
+    suspend fun fetchPokemonImageUrl(id: Int) =
+        withContext(ioDispatcher) {
+            pokemonApi
+                .fetchPokemon(id)
+                .sprites
+                .front_default
+        }
+
+    suspend fun fetchPokemon(id: Int) =
+        withContext(ioDispatcher) {
+            pokemonApi
+                .fetchPokemon(id)
+                .let {
+                    PokemonDetails(
+                        name = it.name,
+                        defaultUrl = it.sprites.front_default
+                    )
+                }
         }
 }
+
+
